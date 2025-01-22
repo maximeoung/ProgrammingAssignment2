48b3b3c7417462b76ec603dbb8750a4959bcb532
maximeoung <maxime.oung@yahoo.fr>
2025-01-22 20:47:48 +0100

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,51 @@
+## function makeCacheMatrix will store a matrix in cache
 makeCacheMatrix <- function(x = matrix()) {
+	## initialize inverse matrix to NULL
 	i <- NULL
+	
+	## set matrix function definition
 	set <- function(y) {
 		x <<- y
 		i <<- NULL
 	}
+	
+	## get matrix function definition
 	get <- function() x
+
+	## set inverse matrix function definition
 	setinverse <- function(inverse) i <<- inverse
+	
+	## get inverse matrix function definition
 	getinverse <- function() i
+
+	## returns list of 'set', 'get', 'setinverse', 'getinverse'
 	list(set = set, get = get,
 		setinverse = setinverse,
 		getinverse = getinverse)
 }
 
-## Write a short comment describing this function
-
+## function 'cacheSolve', when provided a cached matrix created with function 'makeCacheMatrix', will calculate the inverse of the matrix, if it hasn't been calculated already.
+## Else, it will get the inverse matrix that has already been calculated.
+## assume that the matrix supplied is always invertible.
 cacheSolve <- function(x) {
-	## Return a matrix that is the inverse of 'x'
+	## get the 'getinverse' method of cached matrix
 	i <- x$getinverse()
+	
+	## if already calculated, prompt a message 'getting cached data' and returns cached inverse matrix
 	if(!is.null(i)) {
 		message("getting cached data")
 		return(i)
 	}
+	
+	## else we calculate the inverse matrix
 	matrix <- x$get()
 	i <- solve(matrix)
 	x$setinverse(i)
 	i
 }
 
-## verifying functions
+
+## verifying functions :
 ## initialize square matrix  of random integers
 random_matrix <- matrix(round(runif(n=100, min=0, max=9),0), nrow=10)
 
